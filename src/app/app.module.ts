69652f970a19d9a369b9890686c931449c7d935f
Cynthiataragon <cynthiataragon@gmail.com>
2018-05-01 15:13:14 +0300

    Adding the angular 5 files and creating the form in the landingpage.html

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingpageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
